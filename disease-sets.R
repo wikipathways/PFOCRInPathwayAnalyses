09f372dca4564e6dedaa506c835f9a04395b4817
Alexander Pico <apico@gladstone.ucsf.edu>
2020-08-25 12:43:08 -0700

    WikiPathways disease terms

--- disease-sets.R
+++ disease-sets.R
@@ -1,3 +1,35 @@
 # Identify disease ontology terms tagging WikiPathways and PFOCR content,
 # then intersect with Harmonizome datasets
 
+library(tidyverse)
+
+## Identify disease terms used for WikiPathways
+library(rWikiPathways)
+
+# get all human pathways tagged with any disease ontology term
+disease.pathways <- rWikiPathways::getPathwayIdsByParentOntologyTerm("DOID:4")
+hs.pathways <- rWikiPathways::listPathwayIds("Homo sapiens")
+hs.disease.pathways <- intersect(disease.pathways, hs.pathways)
+
+# examine disease terms among these pathways
+all.terms <- lapply(hs.disease.pathways, function(p){
+  ont <- getOntologyTerms(p)
+  # transform into desired df
+  df <- as.data.frame(unlist(ont), stringsAsFactors = F)
+  names(df) <- 'terms'
+  df2 <- df %>%
+    mutate(key = rep(names(ont[[1]]), n() / 3), 
+         id = cumsum(key == names(ont[[1]])[[1]])) %>% 
+    spread(key, terms)
+  
+  df2 %>%
+    filter(ontology == 'Disease') %>%
+    mutate(wpid = p, .before = 1)
+
+
+})
+# combine into single df of WikiPathways-associated disease terms
+all.wp.terms <- bind_rows(all.terms)
+
+# 56 unique disease terms annotating WikiPathways
+unique(all.terms$name)
