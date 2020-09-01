ae944e4c113b69878d217844741c782e3973c1b0
Alexander Pico <apico@gladstone.ucsf.edu>
2020-09-01 13:45:30 -0700

    export disease term lists

--- disease-sets.R
+++ disease-sets.R
@@ -127,18 +127,151 @@ unique(ds$dataset_annot)
 # Intersection of disease terms
 
 ## WP and PFOCR
-wp.pf.terms.1 <- lapply(unique(cur.wp.terms$term), function(p){
-  hits <- agrep(p, unique(cur.pf.terms$term), max = 1, ignore.case = T)
-  if(length(hits) > 0){
-  data.frame(wp.term = p, pf.term.index = hits) %>%
-    mutate(pf.term.name =  unique(cur.pf.terms$term)[pf.term.index]) %>%
-    select(c(1,3))
-  }
-})
-wp.pf.terms.1 <- bind_rows(wp.pf.terms.1)
+# wp.pf.terms.1 <- lapply(unique(cur.wp.terms$term), function(p){
+#   hits <- agrep(p, unique(cur.pf.terms$term), max = 1, ignore.case = T)
+#   if(length(hits) > 0){
+#   data.frame(wp.term = p, pf.term.index = hits) %>%
+#     mutate(pf.term.name =  unique(cur.pf.terms$term)[pf.term.index]) %>%
+#     select(c(1,3))
+#   }
+# })
+# wp.pf.terms.1 <- bind_rows(wp.pf.terms.1)
+# wp.pf.terms.selected <- wp.pf.terms.1 %>%
+#   filter(!wp.term %in% c("Cancer"))
 
-wp.pf.terms.selected <- wp.pf.terms.1 %>%
-  filter(!wp.term %in% c("Cancer"))
+
+# 56 disease terms for WikiPathways
+unique(cur.wp.terms$term)
+# [1] Age related macular degeneration                Aicardi-Goutieres syndrome                     
+# [3] Alzheimer's disease                             Amyloidosis                                    
+#  [5] Arrhythmogenic right ventricular cardiomyopathy Asphyxiating thoracic dystrophy                
+#  [7] Atrial heart septal defect                      Autism spectrum disorder                       
+#  [9] Autosomal dominant cerebellar ataxia            Bardet-Biedl syndrome                          
+# [11] Breast cancer                                   Cancer                                         
+# [13] Cardiomyopathy                                  Charcot-Marie-Tooth disease                    
+# [15] Chromosome 1q21.1 deletion syndrome             Cockayne syndrome                              
+# [17] Congenital disorder of glycosylation            Congenital hypothyroidism                      
+# [19] Craniosynostosis                                cytochrome-c oxidase deficiency disease        
+# [21] Dilated cardiomyopathy                          Ehlers-Danlos syndrome                         
+# [23] Epilepsy                                        Familial atrial fibrillation                   
+# [25] Familial hypertrophic cardiomyopathy            Fanconi anemia                                 
+# [27] Glycogen storage disease                        hemolytic-uremic syndrome                      
+# [29] Idiopathic pulmonary fibrosis                   Intellectual disability                        
+# [31] Joubert syndrome                                Klinefelter's syndrome                         
+# [33] Leber congenital amaurosis                      Leigh disease                                  
+# [35] Long QT syndrome                                Meckel syndrome                                
+# [37] Microphthalmia                                  Mitochondrial complex I deficiency             
+# [39] Mitochondrial complex III deficiency            Mucopolysaccharidosis                          
+# [41] Nephronophthisis                                Neurodegenerative disease                      
+# [43] Neuropathy                                      Noonan syndrome                                
+# [45] Opiate dependence                               Osteogenesis imperfecta                        
+# [47] Osteopetrosis                                   Parkinson's disease                            
+# [49] Porphyria                                       Primary ciliary dyskinesia                     
+# [51] Rheumatoid arthritis                            Schizophrenia                                  
+# [53] Senior-Loken syndrome                           Systemic lupus erythematosus                   
+# [55] Thrombophilia                                   Xeroderma pigmentosum 
+
+## 64 disease terms for PFOCR
+unique(cur.pf.terms$term)
+# [1] 46 XY gonadal dysgenesis                        Age related macular degeneration               
+# [3] Aicardi-Goutieres syndrome                      Alzheimer's disease                            
+#  [5] Amyotrophic lateral sclerosis                   Arrhythmogenic right ventricular cardiomyopathy
+#  [7] Asphyxiating thoracic dystrophy                 Autism spectrum disorder                       
+#  [9] Autoimmune lymphoproliferative syndrome         Autosomal dominant cerebellar ataxia           
+# [11] Autosomal recessive congenital ichthyosis       Breast cancer                                  
+# [13] CAKUT                                           Cancer                                         
+# [15] Cardiomyopathy                                  Charcot-Marie-Tooth disease                    
+# [17] Cholangiocarcinoma                              Cockayne syndrome                              
+# [19] Coenzyme Q10 deficiency disease                 cone-rod dystrophy                             
+# [21] Congenital disorder of glycosylation            Congenital myasthenic syndrome                 
+# [23] Craniosynostosis                                Crohn's disease                                
+# [25] cytochrome-c oxidase deficiency disease         Diabetes mellitus                              
+# [27] Dilated cardiomyopathy                          DOID:12252                                     
+# [29] Dyskeratosis congenita                          Familial hypertrophic cardiomyopathy           
+# [31] Fanconi anemia                                  Glycogen storage disease                       
+# [33] hemolytic-uremic syndrome                       Holoprosencephaly                              
+# [35] Intellectual disability                         Joubert syndrome                               
+# [37] Juvenile rheumatoid arthritis                   Kallmann syndrome                              
+# [39] Leber congenital amaurosis                      limb-girdle muscular dystrophy                 
+# [41] Lung cancer                                     Lynch syndrome                                 
+# [43] Melanoma                                        Nephronophthisis                               
+# [45] Neurodegenerative disease                       Neuropathy                                     
+# [47] Noonan syndrome                                 Obesity                                        
+# [49] Osteopetrosis                                   Parkinson's disease                            
+# [51] Peroxisomal disease                             Polycystic ovary syndrome                      
+# [53] Pontocerebellar hypoplasia                      Porphyria                                      
+# [55] Premature ovarian failure                       Primary pulmonary hypertension                 
+# [57] Retinitis pigmentosa                            Rheumatoid arthritis                           
+# [59] Right atrial isomerism                          Severe combined immunodeficiency               
+# [61] Severe congenital neutropenia                   Systemic lupus erythematosus                   
+# [63] Xeroderma pigmentosum                           Zellweger syndrome  
+
+
+## 33 disease terms in intersection of WP and PFOCR
+cur.intersect.terms <- unique(intersect(cur.wp.terms$term, cur.pf.terms$term))
+# [1] Age related macular degeneration                Aicardi-Goutieres syndrome                     
+# [3] Alzheimer's disease                             Arrhythmogenic right ventricular cardiomyopathy
+#  [5] Asphyxiating thoracic dystrophy                 Autism spectrum disorder                       
+#  [7] Autosomal dominant cerebellar ataxia            Breast cancer                                  
+#  [9] Cardiomyopathy                                  Charcot-Marie-Tooth disease                    
+# [11] Cockayne syndrome                               Congenital disorder of glycosylation           
+# [13] Craniosynostosis                                cytochrome-c oxidase deficiency disease        
+# [15] Dilated cardiomyopathy                          Familial hypertrophic cardiomyopathy           
+# [17] Fanconi anemia                                  Glycogen storage disease                       
+# [19] hemolytic-uremic syndrome                       Intellectual disability                        
+# [21] Joubert syndrome                                Leber congenital amaurosis                     
+# [23] Nephronophthisis                                Neurodegenerative disease                      
+# [25] Neuropathy                                      Noonan syndrome                                
+# [27] Osteopetrosis                                   Parkinson's disease                            
+# [29] Porphyria                                       Rheumatoid arthritis                           
+# [31] Systemic lupus erythematosus                    Xeroderma pigmentosum   
+
+## 87 disease terms in union of WP and PFOCR
+cur.union.terms <- unique(union(cur.wp.terms$term, cur.pf.terms$term))
+# [1] "Age related macular degeneration"                "Aicardi-Goutieres syndrome"                     
+# [3] "Alzheimer's disease"                             "Amyloidosis"                                    
+# [5] "Arrhythmogenic right ventricular cardiomyopathy" "Asphyxiating thoracic dystrophy"                
+# [7] "Atrial heart septal defect"                      "Autism spectrum disorder"                       
+# [9] "Autosomal dominant cerebellar ataxia"            "Bardet-Biedl syndrome"                          
+# [11] "Breast cancer"                                   "Cancer"                                         
+# [13] "Cardiomyopathy"                                  "Charcot-Marie-Tooth disease"                    
+# [15] "Chromosome 1q21.1 deletion syndrome"             "Cockayne syndrome"                              
+# [17] "Congenital disorder of glycosylation"            "Congenital hypothyroidism"                      
+# [19] "Craniosynostosis"                                "cytochrome-c oxidase deficiency disease"        
+# [21] "Dilated cardiomyopathy"                          "Ehlers-Danlos syndrome"                         
+# [23] "Epilepsy"                                        "Familial atrial fibrillation"                   
+# [25] "Familial hypertrophic cardiomyopathy"            "Fanconi anemia"                                 
+# [27] "Glycogen storage disease"                        "hemolytic-uremic syndrome"                      
+# [29] "Idiopathic pulmonary fibrosis"                   "Intellectual disability"                        
+# [31] "Joubert syndrome"                                "Klinefelter's syndrome"                         
+# [33] "Leber congenital amaurosis"                      "Leigh disease"                                  
+# [35] "Long QT syndrome"                                "Meckel syndrome"                                
+# [37] "Microphthalmia"                                  "Mitochondrial complex I deficiency"             
+# [39] "Mitochondrial complex III deficiency"            "Mucopolysaccharidosis"                          
+# [41] "Nephronophthisis"                                "Neurodegenerative disease"                      
+# [43] "Neuropathy"                                      "Noonan syndrome"                                
+# [45] "Opiate dependence"                               "Osteogenesis imperfecta"                        
+# [47] "Osteopetrosis"                                   "Parkinson's disease"                            
+# [49] "Porphyria"                                       "Primary ciliary dyskinesia"                     
+# [51] "Rheumatoid arthritis"                            "Schizophrenia"                                  
+# [53] "Senior-Loken syndrome"                           "Systemic lupus erythematosus"                   
+# [55] "Thrombophilia"                                   "Xeroderma pigmentosum"                          
+# [57] "46 XY gonadal dysgenesis"                        "Amyotrophic lateral sclerosis"                  
+# [59] "Autoimmune lymphoproliferative syndrome"         "Autosomal recessive congenital ichthyosis"      
+# [61] "CAKUT"                                           "Cholangiocarcinoma"                             
+# [63] "Coenzyme Q10 deficiency disease"                 "cone-rod dystrophy"                             
+# [65] "Congenital myasthenic syndrome"                  "Crohn's disease"                                
+# [67] "Diabetes mellitus"                               "DOID:12252"                                     
+# [69] "Dyskeratosis congenita"                          "Holoprosencephaly"                              
+# [71] "Juvenile rheumatoid arthritis"                   "Kallmann syndrome"                              
+# [73] "limb-girdle muscular dystrophy"                  "Lung cancer"                                    
+# [75] "Lynch syndrome"                                  "Melanoma"                                       
+# [77] "Obesity"                                         "Peroxisomal disease"                            
+# [79] "Polycystic ovary syndrome"                       "Pontocerebellar hypoplasia"                     
+# [81] "Premature ovarian failure"                       "Primary pulmonary hypertension"                 
+# [83] "Retinitis pigmentosa"                            "Right atrial isomerism"                         
+# [85] "Severe combined immunodeficiency"                "Severe congenital neutropenia"                  
+# [87] "Zellweger syndrome"   
 
 ## WP and Data
 wp.data.terms.1 <- lapply(unique(cur.wp.terms$term), function(p){
