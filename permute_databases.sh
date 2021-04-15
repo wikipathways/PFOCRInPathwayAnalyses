44bb30b712296b3ed18f1fe820403e71eaa21b77
reubenthomas <dr.reuben.thomas@gmail.com>
2021-04-15 11:38:22 -0700

    update

--- permute_databases.sh
+++ permute_databases.sh
@@ -4,16 +4,16 @@
 #$ -l mem_free=5G
 #$ -l scratch=50G
 #$ -l h_rt=24:00:00
-#$ -t 1-2
+#$ -t 101-536
 scriptDir=/wynton/group/gladstone/biocore/projects/pfocr_pathway_enrichment_evaluation/PFOCRInPathwayAnalyses/
 containerDir=/wynton/group/gladstone/biocore/containers
 dataDir=/wynton/group/gladstone/biocore/projects/PFOCR/
-dataDir1=/wynton/group/gladstone/biocore/projects/pfocr_pathway_enrichment_evaluation/
+dataDir1=/wynton/group/gladstone/biocore/projects/pfocr_pathway_enrichment_evaluation/permuted_geneset_databases_results_remove_rSEA3/
 export SINGULARITY_BINDPATH="$containerDir,$scriptDir,$dataDir,$dataDir1"
 
 
 f=$1
-singularity exec $containerDir/pathway_enrichment_pathway_databases_evaluation_latest.sif Rscript permuted_gene_sets.R $f
+singularity exec $containerDir/pathway_enrichment_pathway_databases_evaluation_latest.sif Rscript permuted_gene_sets.R $SGE_TASK_ID
 
 [[ -n "$JOB_ID" ]] && qstat -j "$JOB_ID"
 
